59a97da07e4f37041d8d3223e0e1e07e9b5b1ba1
shao <shaoguotong@yunzujia.com>
2016-03-17 16:55:11 +0800

    no message

---
+++ Assets/flappyBird/scripts/isReady.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isReady : MonoBehaviour {
+	public GameObject road, bird;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			gameObject.SetActive(false);
+			road.GetComponent<road>().isBegin = true;
+			bird.GetComponent<Rigidbody2D>().isKinematic = false;
+			bird.GetComponent<Animator>().enabled = false;
+
+			#if UNITY_ANDROID
+			using(AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer")){
+				using(AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity")){
+					jo.Call("start");
+				}
+			}
+			#endif
+		}
+	}
+}
